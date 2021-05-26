1cac3e7bb7437f88ce11d0ac3ce5d15e1088b49f
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-05-26 18:22:15 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/germanocorbolan/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.germanocorbolan.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.germanocorbolan.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
