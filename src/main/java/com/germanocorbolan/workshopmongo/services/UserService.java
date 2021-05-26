d3ef2fccda036c1b02596b247aed87d937dee296
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-05-25 21:47:50 -0300

    Obtendo um usuário por id

--- src/main/java/com/germanocorbolan/workshopmongo/services/UserService.java
+++ src/main/java/com/germanocorbolan/workshopmongo/services/UserService.java
@@ -1,24 +1,26 @@
 package com.germanocorbolan.workshopmongo.services;
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.germanocorbolan.workshopmongo.domain.User;
 import com.germanocorbolan.workshopmongo.repository.UserRepository;
+import com.germanocorbolan.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 	
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
 	
-	
-	
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
